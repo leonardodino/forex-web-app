47996cc88ae4e2d2b6b87e7934511aa4f945802d
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-02-24 05:30:25 -0300

    :sparkles: FlipButton

---
+++ src/components/FlipButton.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import styled from 'styled-components'
+import { fg, base, invert } from '../utils/theme'
+
+const gap = '0.375rem'
+const border = '1px solid'
+const fontSize = '0.75rem'
+
+const Wrapper = styled.button`
+  ${base}
+  appearance: none;
+  font-family: inherit;
+  padding: 0 ${gap};
+  display: flex;
+  border: ${border} ${fg};
+  border-radius: 4px;
+  align-items: center;
+`
+
+const Icon = ({ color = 'currentColor', strokeWidth = 2, ...props }) => (
+  <svg
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke={color}
+    strokeWidth={strokeWidth}
+    {...props}
+  >
+    <polyline points='16 3 21 3 21 8' />
+    <line x1='4' y1='20' x2='21' y2='3' />
+    <polyline points='21 16 21 21 16 21' />
+    <line x1='15' y1='15' x2='21' y2='21' />
+    <line x1='4' y1='4' x2='9' y2='9' />
+  </svg>
+)
+
+const StyledIcon = styled(Icon)`
+  height: ${fontSize};
+  width: ${fontSize};
+  margin: ${gap} 0;
+  display: block;
+  flex: 0 0 auto;
+`
+
+const Rate = styled.div`
+  font-weight: 500;
+  font-size: ${fontSize};
+  line-height: 1rem;
+  padding: 0.25rem 0 0.25rem ${gap};
+  border-left: ${border} ${fg};
+  margin-left: ${gap};
+  flex: 0 0 auto;
+`
+
+const FlipButton = ({ rate, ...props }) =>
+  typeof rate === 'number' && (
+    <Wrapper type='button' {...props}>
+      <StyledIcon />
+      <Rate>{rate}</Rate>
+    </Wrapper>
+  )
+
+export default invert(FlipButton)
