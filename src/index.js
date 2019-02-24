9a31c50c5725bbf4b47edcaa58d40f5aa129e38b
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-02-24 03:51:39 -0300

    :lipstick: add proper css reset

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import 'minireset.css'
 import './index.css'
 import App from './components/App'
 import * as serviceWorker from './serviceWorker'
