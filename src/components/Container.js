6c2918b3de47a6c9ac87fea0d39620a9005a1dc6
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-02-24 05:30:25 -0300

    :sparkles: Container

---
+++ src/components/Container.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 48rem;
+  padding: 0 0.75rem;
+  margin: 0 auto;
+`
+
+export default Container
