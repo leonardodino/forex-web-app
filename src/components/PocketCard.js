639405b1e643a55003c0e6316aa1e5ef1b07a130
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-02-24 05:30:25 -0300

    :lipstick: PocketCard

--- src/components/PocketCard.js
+++ src/components/PocketCard.js
@@ -1,46 +1,87 @@
 import React from 'react'
 import styled from 'styled-components'
-import { base, invert } from '../utils/theme'
+import { fg, base, invert } from '../utils/theme'
 import { format } from '../utils/dinero'
 import { useCurrencyForex } from '../hooks/forex'
 
-const HeaderWrapper = styled.div`
+const Wrapper = styled.div`
+  display: inline-block;
+  overflow: hidden;
+  border-radius: 0.25rem;
+  border: 1px solid ${fg};
+  margin: 0.75rem;
+`
+
+const Flex = styled.div`
   ${base}
-  font-size: 2rem;
+  flex: 1;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `
 
-const Header = invert(({ currency, value }) => (
-  <HeaderWrapper>
-    <div>{currency}</div>
-    <div>{value}</div>
-  </HeaderWrapper>
+const Currency = styled.h3`
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: 600;
+  padding: 0.75rem 0.5rem;
+`
+
+const Amount = styled.div`
+  font-weight: 400;
+  font-size: 1.5rem;
+  line-height: 1.5rem;
+  padding: 0.75rem 0.5rem;
+  letter-spacing: -0.025rem;
+`
+
+const ForexWrapper = styled(Flex)`
+  border-left: 1px solid ${fg};
+  &:first-child {
+    border: 0;
+  }
+`
+
+const ForexCurrency = styled.div`
+  margin: 0.75rem 0.5rem;
+  font-size: 0.75rem;
+  line-height: 1rem;
+  font-weight: 600;
+`
+const ForexRate = styled.div`
+  margin: 0.75rem 0.5rem;
+  font-size: 0.875rem;
+  line-height: 1rem;
+`
+
+const Header = invert(({ currency, amount }) => (
+  <Flex>
+    <Currency>{currency}</Currency>
+    <Amount>{amount}</Amount>
+  </Flex>
 ))
 
-const RateWrapper = styled.div`
-  ${base}
-  display: flex;
-  justify-content: space-between;
-`
-
-const Rate = ({ currency, rate }) => (
-  <RateWrapper>
-    <div>{currency}</div>
-    <div>{rate}</div>
-  </RateWrapper>
+const Forex = ({ currency, rate }) => (
+  <ForexWrapper>
+    <ForexCurrency>{currency}</ForexCurrency>
+    <ForexRate>{rate}</ForexRate>
+  </ForexWrapper>
 )
 
 const PocketCard = ({ fund }) => {
   const [formatted, currency] = [format(fund), fund.getCurrency()]
   const { rates } = useCurrencyForex(currency)
   return (
-    <div>
-      <Header currency={currency} value={formatted} />
-      {Object.entries(rates).map(([currency, rate]) => (
-        <Rate key={currency} currency={currency} rate={rate} />
-      ))}
-    </div>
+    <Wrapper>
+      <Header currency={currency} amount={formatted} />
+      {!!rates && (
+        <Flex>
+          {Object.entries(rates).map(([currency, rate]) => (
+            <Forex key={currency} currency={currency} rate={rate} />
+          ))}
+        </Flex>
+      )}
+    </Wrapper>
   )
 }
 
