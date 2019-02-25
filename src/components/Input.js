e50125d22538e31198b413d37b5bb9b49b687569
Leonardo Dino <leonardo@kunstdesign.com.br>
2019-02-25 01:15:13 -0300

    :sparkles: Input

---
+++ src/components/Input.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+import { base } from '../utils/theme'
+
+const Input = styled.input`
+  ${base}
+  appearance: none;
+  caret-color: currentColor;
+  padding: 0;
+  text-align: right;
+  border: 0px solid transparent;
+  border-radius: 0.25rem;
+  font-size: 2rem;
+  line-height: 2rem;
+  font-weight: 500;
+  letter-spacing: -0.025em;
+  min-width: 10rem;
+  flex: 0 1 autos;
+`
+
+export default Input
